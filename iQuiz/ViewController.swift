1342c69961fc7ad9eaa165e38a75c80b3c2b62ea
Abiscula <victorabiscula@live.com>
2024-02-29 00:36:21 -0300

    first commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Victor Abiscula on 28/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}	

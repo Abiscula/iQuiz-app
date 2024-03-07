c62dd9b97fa4c4dadc0dd2b29016cc678eebc29a
Abiscula <victorabiscula@live.com>
2024-03-07 20:27:20 -0300

    Adicionado novos estilos ao modelo de question
    Incluido retorno visual para o usuario ao responder uma pergunta

--- iQuiz/QuestionViewController.swift
+++ iQuiz/QuestionViewController.swift
@@ -9,28 +9,52 @@ import UIKit
 
 class QuestionViewController: UIViewController {
   
+    var points = 0
+    var questionNumber = 0
+  
     @IBOutlet weak var questionTitleLabel: UILabel!
-  
-  
     @IBOutlet var respsBtn: [UIButton]!
-    
-    @IBAction func pressRespBtn(_ sender: UIButton) {
-      print(sender.tag)
-    }
   
+    @IBAction func pressRespBtn(_ sender: UIButton) {
+      let userisCorrect = questions[questionNumber].correctAnswer == sender.tag
+      
+      if(userisCorrect) {
+        points += 1
+        sender.backgroundColor = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1.0)
+      } else {
+        sender.backgroundColor = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1.0)
+      }
+      
+      if questionNumber < questions.count - 1 {
+        questionNumber += 1
+        questionConfig()
+      }
+      
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutConfig()
+        questionConfig()
     }
   
   func layoutConfig() {
       navigationItem.hidesBackButton = true
-    
+      questionTitleLabel.numberOfLines = 0
+      questionTitleLabel.textAlignment = .center
       for button in respsBtn{
         button.layer.cornerRadius = 12.0
       }
     }
-    
+  
+  func questionConfig() {
+    questionTitleLabel.text = questions[questionNumber].title
+    for button in respsBtn {
+      let btnTitle = questions[questionNumber].responses[button.tag]
+      button.setTitle(btnTitle, for: .normal)
+      button.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+    }
+  }
   
   /*
     // MARK: - Navigation
